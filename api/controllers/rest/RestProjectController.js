549b0d15ae962cc29751914366f72c19f4bb7ded
Amitav Roy <reachme@amitavroy.com>
2016-05-11 15:47:49 +0530

    Project with client selection is working.

--- api/controllers/rest/RestProjectController.js
+++ api/controllers/rest/RestProjectController.js
@@ -5,19 +5,19 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 module.exports = {
-  restGetProjects: function (req, res) {
+  restGetProjects: function(req, res) {
     Project.find({
       limit: 20
-    }).exec(function (err, projects) {
+    }).exec(function(err, projects) {
       return res.json(200, projects);
     });
   },
-  saveProject: function (req, res) {
+  saveProject: function(req, res) {
     Project.create({
       name: req.body.display_name,
-      client_id: req.body.client_id,
+      client_id: req.body.client.id,
       status: 1
-    }).exec(function (err, project) {
+    }).exec(function(err, project) {
       if (!err) {
         sails.sockets.blast('project-added', {
           project: project
